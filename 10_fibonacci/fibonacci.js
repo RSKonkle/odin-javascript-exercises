41037d4703685855b5d41a804454c425aa4c03cc
Reagan Konkle <regosmallz@gmail.com>
2024-08-22 16:39:16 -0400

    Added function output, enabled testing

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    x = parseInt(x)
 
+    if (x == 0)
+        return 0;
+    else if (x < 0)
+        return "OOPS";
+
+    else {
+        let seq = [1,1]
+        for (let i = 2; i <= x; i++)
+            seq[i] = seq[i-2] + seq[i-1];
+        return seq[x-1]
+     };
 };
 
 // Do not edit below this line
