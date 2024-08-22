27f3b5e73015518cdf9bc25500b35d1c49f68728
Reagan Konkle <regosmallz@gmail.com>
2024-08-22 14:28:04 -0400

    Added function output, enabled testing

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,44 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y
 };
 
-const sum = function() {
-	
+const sum = function(xArray) {
+  result = 0
+	for (let i = 0; i < xArray.length; i++)
+    result += xArray[i];
+  return result
 };
 
-const multiply = function() {
-
+const multiply = function(xArray) {
+  result = 1
+  for (let i = 0; i < xArray.length; i++)
+    result *= xArray[i];
+  return result
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	result = x
+  for (let i = 1; i < y; i++)
+    result *= x;
+  return result
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+  if (x < 0)
+    return -1;
+  else if (x == 1 || x == 0)
+    return 1;
+  
+  else {
+    result = x
+    for (let i = x - 1; i >= 1; i--)
+      result *= i;
+    return result;
+  };
 };
 
 // Do not edit below this line
